581da946aa46d6d3235662d16d02acc6208a752a
Georgina Zamboni <Geor2015zamboni@gmail.com>
2026-02-05 18:55:43 -0300

    Estilizado de navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+
 import ItemListContainer from './componentes/ItemListContainer'
 import NavBar from './componentes/NavBar'
 
--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,19 +1,26 @@
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import CartWidget from "./CartWidget"
 
 const NavBar =()=>{
     return(
         <nav className="nav-container">
-            <a className="nav-link" href="">
+            <div className='nav-logo'>
+              <a className="nav-link" href="">
                 <img src={'../logo-akari-rojo.png'} alt="Logo Akari Store" width={180} />
-            </a>
-           {/* Lo dejo así por ahora, despues lo quiero hacer un desplegable que se llame categorías 
-           y dentro tenga a figuras, cuadros y remeras */}
-            <a className="nav-link" href="">Figuras</a>
-            <a className="nav-link" href="">Cuadros</a>
-            <a className="nav-link" href="">Remeras</a>
-            <a className="nav-link" href="">Envíos</a>
-            <a className="nav-link" href="">Contacto</a>
-            <CartWidget />
+              </a>
+            </div>
+            <div className="nav-links">
+              <NavDropdown title="Categorías" id="basic-nav-dropdown">
+                <NavDropdown.Item href="">Figuras de acción</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="">Cuadros decorativos</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="">Remeras estampadas</NavDropdown.Item>
+              </NavDropdown>
+              <a className="nav-link" href="">Envíos</a>
+              <a className="nav-link" href="">Contacto</a>
+              <CartWidget />
+            </div>  
         </nav>
     )
 }
