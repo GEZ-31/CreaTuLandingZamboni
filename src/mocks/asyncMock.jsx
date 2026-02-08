7c5671dbcad6bbe4b48924c915737bc5c837b669
Georgina Zamboni <Geor2015zamboni@gmail.com>
2026-02-08 00:26:32 -0300

    Carga de productos en mock completada

--- src/mocks/asyncMock.jsx
+++ src/mocks/asyncMock.jsx
@@ -44,6 +44,249 @@ const productos = [
         price: 63000,
         category: 'cuadros',
         img: 'https://i.postimg.cc/CLsBBGgV/cua5.jpg'
+    },
+    {
+        id: '06',
+        name: 'Figura AT1',
+        description: 'Figura de Eren Jaeger versión titán, personaje principal de Attack On Titan. La figura mide 30cm de altura y está hecha de PVC.',
+        stock: 30,
+        price: 95000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/sDs1rMZh/atk.jpg'
+    },
+    {
+        id: '07',
+        name: 'Figura AT2',
+        description: 'Figura de Eren Jaeger versión legión, personaje principal de Attack On Titan. La figura mide 30cm de altura y está hecha de PVC.',
+        stock: 25,
+        price: 90000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/L62hpJZJ/atk_2.jpg'
+    },
+    {
+        id: '08',
+        name: 'Figura DB1',
+        description: 'Figura de Goku versión Super Saiyajin, personaje principal de Dragon Ball. La figura mide 25cm de altura y está hecha de PVC.',
+        stock: 40,
+        price: 85000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/bNpdhZ2D/db.jpg'
+    },
+    {
+        id: '09',
+        name: 'Figura DB2',
+        description: 'Figura de Vegeta, personaje principal de Dragon Ball. La figura mide 25cm de altura y está hecha de PVC.',
+        stock: 35,
+        price: 80000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/Hsdjgr78/db3.jpg'
+    },
+    {
+        id: '10',
+        name: 'Figura DB3',
+        description: 'Figura de Majin Boo, personaje principal de Dragon Ball. La figura mide 25cm de altura y está hecha de PVC.', 
+        stock: 20,
+        price: 75000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/PrTNjC8Y/db7.jpg'
+    },
+    {
+        id: '11',
+        name: 'Figura DS1',
+        description: 'Figura de Tanjiro, personaje principal de Demon Slayer. La figura mide 25cm de altura y está hecha de PVC.',
+        stock: 25,
+        price: 85000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/FsN14YJj/demon_3.jpg'
+    },
+    {
+        id: '12',
+        name: 'Figura DS2',
+        description: 'Figura de Tanjiro y Nezuko, personajes principales de Demon Slayer. La figura mide 25cm de altura y está hecha de PVC.',
+        stock: 30,
+        price: 120000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/3JYWTkDX/demon_7.jpg'
+    },
+    {
+        id: '13',
+        name: 'Figura NS1',
+        description: 'Figura de Naruto, personaje principal de Naruto. La figura mide 25cm de altura y está hecha de PVC.',
+        stock: 40,
+        price: 85000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/8PNsS7rL/naruto_2.jpg'
+    },
+    {
+        id: '14',
+        name: 'Figura NS2',
+        description: 'Figura de Itachi, personaje principal de Naruto. La figura mide 25cm de altura y está hecha de PVC.',
+        stock: 35,
+        price: 98000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/kXqDm2tv/naruto_3.jpg'
+    },
+    {
+        id: '15',
+        name: 'Figura NS3',
+        description: 'Figura de Itachi y Sasuke, personajes principales de Naruto. La figura mide 25cm de altura y está hecha de PVC.',
+        stock: 20,
+        price: 115000,
+        category: 'figuras',
+        img: 'https://i.postimg.cc/15s4Q8NK/naruto_5.jpg'
+    },
+    {
+        id: '16',
+        name: 'Peluche PK1',
+        description: 'Peluche del Pokemon Bulbasor. El peluche mide 20cm de altura y está hecho de algodón.',
+        stock: 30,
+        price: 12000,
+        category: 'peluches',
+        img: 'https://i.postimg.cc/g20Jx1s6/peluche_bulbasor.jpg'
+    },
+    {
+        id: '17',
+        name: 'Peluche PK2',
+        description: 'Peluche del Pokemon Eevee. El peluche mide 20cm de altura y está hecho de algodón.',
+        stock: 25,
+        price: 12000,
+        category: 'peluches',
+        img: 'https://i.postimg.cc/ht3GTMZ1/peluche_eeve.jpg'
+    },
+    {
+        id: '18',
+        name: 'Peluche PK3',
+        description: 'Peluche del Pokemon Lapras. El peluche mide 20cm de altura y está hecho de algodón.',
+        stock: 30,
+        price: 12000,
+        category: 'peluches',
+        img: 'https://i.postimg.cc/W1f4gSY8/peluche_lapras.jpg'
+    },
+    {
+        id: '19',
+        name: 'Peluche PK4',
+        description: 'Peluche del Pokemon Marowak. El peluche mide 20cm de altura y está hecho de algodón.',
+        stock: 25,
+        price: 12000,
+        category: 'peluches',
+        img: 'https://i.postimg.cc/rwYpxgZg/peluche_marowak.jpg'
+    },
+    {
+        id: '20',
+        name: 'Peluche PK5',
+        description: 'Peluche del Pokemon Charizard. El peluche mide 20cm de altura y está hecho de algodón.',
+        stock: 30,
+        price: 12000,
+        category: 'peluches',
+        img: 'https://i.postimg.cc/N09j4TW9/Peluche_Charizard.jpg'
+    },
+    {
+        id: '21',
+        name: 'Peluche PK6',
+        description: 'Peluche del Pokemon Pikachu. El peluche mide 20cm de altura y está hecho de algodón.',
+        stock: 30,
+        price: 12000,
+        category: 'peluches',
+        img: 'https://i.postimg.cc/LsY8TfrJ/peluche_pikachu.jpg'
+    },
+    {
+        id: '22',
+        name: 'Peluche PK7',
+        description: 'Peluche del Pokemon Snorlax. El peluche mide 20cm de altura y está hecho de algodón.',
+        stock: 25,
+        price: 12000,
+        category: 'peluches',
+        img: 'https://i.postimg.cc/MGMpDRhc/peluche_snorlax.jpg'
+    },
+    {
+        id: '23',
+        name: 'Peluche PK8',
+        description: 'Peluche del Pokemon Squirtle. El peluche mide 20cm de altura y está hecho de algodón.',
+        stock: 30,
+        price: 12000,
+        category: 'peluches',
+        img: 'https://i.postimg.cc/vHY8KCJw/peluche_squa.jpg'
+    },
+    {
+        id: '24',
+        name: 'Remera DN1',
+        description: 'Remera estampada con imagen de Death Note. La remera está hecha de algodón, es talle único y es unisex.',
+        stock: 50,
+        price: 7500,
+        category: 'remeras',
+        img: 'https://i.postimg.cc/6q0W3hGY/rem_ele.jpg'
+    },
+    {
+        id: '25',
+        name: 'Remera NS1',
+        description: 'Remera estampada con imagen de Naruto. La remera está hecha de algodón, es talle único y es unisex.',
+        stock: 40,
+        price: 7500,
+        category: 'remeras',
+        img: 'https://i.postimg.cc/yxP1dTSQ/rem_na.jpg'
+    },
+    {
+        id: '26',
+        name: 'Remera NS2',
+        description: 'Remera estampada con imagen de Naruto. La remera está hecha de algodón, es talle único y es unisex.',
+        stock: 38,
+        price: 7500,
+        category: 'remeras',
+        img: 'https://i.postimg.cc/KjNGz5g0/rem_nar.jpg'
+    },
+    {
+        id: '27',
+        name: 'Remera NS3',
+        description: 'Remera estampada con imagen de Naruto. La remera está hecha de algodón, es talle único y es unisex.',
+        stock: 35,
+        price: 7500,
+        category: 'remeras',
+        img: 'https://i.postimg.cc/T19dwcWH/rem_nara.jpg'
+    },
+    {
+        id: '28',
+        name: 'Remera NS4',
+        description: 'Remera estampada con imagen de Naruto. La remera está hecha de algodón, es talle único y es unisex.',
+        stock: 30,
+        price: 7500,
+        category: 'remeras',
+        img: 'https://i.postimg.cc/SRrSsGz5/rem_naru.jpg'
+    },
+    {
+        id: '29',
+        name: 'Remera NS5',
+        description: 'Remera estampada con imagen de Naruto. La remera está hecha de algodón, es talle único y es unisex.',
+        stock: 32,
+        price: 7500,
+        category: 'remeras',
+        img: 'https://i.postimg.cc/VvRf6Fb3/rem_pain.jpg'
+    },
+    {
+        id: '30',
+        name: 'Remera OP1',
+        description: 'Remera estampada con imagen de One Piece. La remera está hecha de algodón, es talle único y es unisex.',
+        stock: 40,
+        price: 7500,
+        category: 'remeras',
+        img: 'https://i.postimg.cc/sxcjXJZb/rem_one.jpg'
+    },
+    {
+        id: '31',
+        name: 'Remera OP2',
+        description: 'Remera estampada con imagen de One Piece. La remera está hecha de algodón, es talle único y es unisex.',
+        stock: 35,
+        price: 7500,
+        category: 'remeras',
+        img: 'https://i.postimg.cc/ZncbRLy2/rem_one_p.jpg'
+    },
+    {
+        id: '32',
+        name: 'Remera DB1',
+        description: 'Remera estampada con imagen de Dragon Ball. La remera está hecha de algodón, es talle único y es unisex.',
+        stock: 40,
+        price: 7500,
+        category: 'remeras',
+        img: 'https://i.postimg.cc/HW4sBTPT/rem_veg.jpg'
     }
 ]
 
